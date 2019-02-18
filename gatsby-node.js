5164f7afee07d86e492f8f124a9a7cfb04de085c
patrikarvidsson <patrik@arvidson.nu>
2019-02-18 16:09:07 +0100

    Adding simple route for journal entries

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    const blogPost = path.resolve('src/templates/blog-post.js')
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulBlogPost {
+              edges {
+                node {
+                  title
+                  slug
+                }
+              }
+            }
+          }
+          `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        const posts = result.data.allContentfulBlogPost.edges
+        posts.forEach((post, index) => {
+          createPage({
+            path: `/journal/${post.node.slug}/`,
+            component: blogPost,
+            context: {
+              slug: post.node.slug
+            },
+          })
+        })
+      })
+    )
+  })
+}
---
+++ src/templates/blog-post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Helmet from 'react-helmet'
+import get from 'lodash/get'
+import Img from 'gatsby-image'
+import Layout from '../components/layout'
+
+
+class BlogPostTemplate extends React.Component {
+  render() {
+    return (
+      <Layout >
+      </Layout>
+    )
+  }
+}
+
+export default BlogPostTemplate
